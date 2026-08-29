5980de9b72c9b3600b3cd7ae4cbb5fdff0aef460
Haixing Hu <starfish.hu@gmail.com>
2026-08-30 02:53:08 +0800

    test: add identifier parser fuzz target

---
+++ fuzz/fuzz_targets/id_parser.rs
@@ -0,0 +1,10 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+use qubit_reflect::identity::{CapabilityId, ExternalTraitId};
+
+// Exercises public identifier parsing with arbitrary UTF-8 input.
+fuzz_target!(|data: String| {
+    let _ = CapabilityId::new(&data);
+    let _ = ExternalTraitId::new(&data);
+});
